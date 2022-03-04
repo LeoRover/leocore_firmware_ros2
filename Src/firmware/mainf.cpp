b7110248c30cac5994e7e7b4571c8df59d5da10d
Błażej Sowa <bsowa123@gmail.com>
2022-03-04 20:51:43 +0100

    Working battery voltage publishing

--- Src/firmware/mainf.cpp
+++ Src/firmware/mainf.cpp
@@ -5,64 +5,97 @@
 #include <rmw_microros/rmw_microros.h>
 #include <uxr/client/transport.h>
 
-#include <std_msgs/msg/int32.h>
+#include <std_msgs/msg/float32.h>
+
+#include "wheel_controller.hpp"
 
 #include "mainf.h"
-#include "usart.h"
 
 #include "microros/uart_transport.h"
 
-rclc_support_t support;
-rcl_node_t node;
-rcl_publisher_t publisher;
+#include "firmware/configuration.hpp"
+#include "firmware/imu_receiver.hpp"
+#include "firmware/parameters.hpp"
+
+static rclc_support_t support;
+static rcl_node_t node;
+static bool configured = false;
+
+static std_msgs__msg__Float32 battery;
+static std_msgs__msg__Float32 battery_averaged;
+static rcl_publisher_t battery_pub;
+static rcl_publisher_t battery_averaged_pub;
+static CircularBuffer<float> battery_buffer_(BATTERY_BUFFER_SIZE);
+static bool publish_battery = false;
+
 rclc_executor_t executor;
-rcl_timer_t timer;
 
-std_msgs__msg__Int32 msg;
-
-void timer_callback(rcl_timer_t* timer, int64_t last_call_time) {
-  RCLC_UNUSED(last_call_time);
-  if (timer != NULL) {
-    rcl_publish(&publisher, &msg, NULL);
-    msg.data++;
-  }
-}
+Parameters params;
 
 void error_loop() {
   for (;;) {
   }
 }
 
-#define RCCHECK(fn)                \
-  {                                \
-    rcl_ret_t temp_rc = fn;        \
-    if ((temp_rc != RCL_RET_OK)) { \
-      error_loop();                \
-    }                              \
-  }
-
 void setup() {
   rmw_uros_set_uart_transport(&huart1);
 
   static rcl_allocator_t allocator = rcl_get_default_allocator();
 
-  RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+  rcl_ret_t rc = rclc_support_init(&support, 0, NULL, &allocator);
+  if (rc != RCL_RET_OK) error_loop();
 
-  RCCHECK(rclc_node_init_default(&node, "firmware", "", &support));
+  rc = rclc_node_init_default(&node, "firmware", "", &support);
+  if (rc != RCL_RET_OK) error_loop();
 
-  RCCHECK(rclc_publisher_init_default(
-      &publisher, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
-      "test"));
+  rclc_publisher_init_default(
+      &battery_pub, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32),
+      "firmware/battery");
+  rclc_publisher_init_default(
+      &battery_averaged_pub, &node,
+      ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32),
+      "firmware/battery_averaged");
 
-  RCCHECK(rclc_timer_init_default(&timer, &support, RCL_MS_TO_NS(1000),
-                                  timer_callback));
+  rclc_executor_init(&executor, &support.context, 1, &allocator);
 
-  RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
-  RCCHECK(rclc_executor_add_timer(&executor, &timer));
-
-  msg.data = 0;
+  configured = true;
 }
 
 void loop() {
-  RCCHECK(rclc_executor_spin(&executor));
+  rcl_ret_t rc = rclc_executor_spin_some(&executor, 1000 * 1000 * 1000);
+  if (rc != RCL_RET_OK) error_loop();
+
+  if (publish_battery) {
+    rcl_publish(&battery_pub, &battery, NULL);
+    rcl_publish(&battery_averaged_pub, &battery_averaged, NULL);
+    publish_battery = false;
+  }
+}
+
+void update() {
+  static uint32_t cnt = 0;
+  ++cnt;
+
+  static float battery_sum = 0.0F;
+  static float battery_avg = 0.0F;
+  float battery_new = static_cast<float>(BATTERY_ADC) * BATTERY_ADC_TO_VOLTAGE;
+  battery_sum += battery_new;
+  battery_sum -= battery_buffer_.push_back(battery_new);
+  battery_avg =
+      battery_sum / static_cast<float>(std::min(BATTERY_BUFFER_SIZE, cnt));
+
+  if (battery_avg < params.battery_min_voltage) {
+    if (cnt % 10 == 0) gpio_toggle(LED);
+  } else {
+    gpio_reset(LED);
+  }
+
+  if (!configured) return;
+
+  if (cnt % BATTERY_PUB_PERIOD == 0 && !publish_battery) {
+    battery.data = battery_new;
+    battery_averaged.data = battery_avg;
+
+    publish_battery = true;
+  }
 }
