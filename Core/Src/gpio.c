be9ae6cfa3fbf2103a0396ce7f8e065446be79e3
Błażej Sowa <bsowa123@gmail.com>
2025-04-24 18:36:31 +0200

    Regenerate code with newer CubeMX

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -63,8 +63,8 @@ void MX_GPIO_Init(void)
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_SET);
 
-  /*Configure GPIO pins : PCPin PCPin PCPin PCPin
-                           PCPin PCPin PCPin */
+  /*Configure GPIO pins : H1_NSLEEP_Pin H1_PHASE_Pin H1_MODE_Pin H3_MODE_Pin
+                           H4_MODE_Pin H4_PHASE_Pin H2_MODE_Pin */
   GPIO_InitStruct.Pin = H1_NSLEEP_Pin|H1_PHASE_Pin|H1_MODE_Pin|H3_MODE_Pin
                           |H4_MODE_Pin|H4_PHASE_Pin|H2_MODE_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
@@ -72,33 +72,33 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PCPin PCPin PCPin PCPin */
+  /*Configure GPIO pins : H2_FAULT_Pin H1_FAULT_Pin H3_FAULT_Pin IMU_INT_2_Pin */
   GPIO_InitStruct.Pin = H2_FAULT_Pin|H1_FAULT_Pin|H3_FAULT_Pin|IMU_INT_2_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PBPin PBPin */
+  /*Configure GPIO pins : IMU_INT_1_Pin H4_FAULT_Pin */
   GPIO_InitStruct.Pin = IMU_INT_1_Pin|H4_FAULT_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PBPin PBPin PBPin PBPin */
+  /*Configure GPIO pins : H3_NSLEEP_Pin H3_PHASE_Pin H2_NSLEEP_Pin H2_PHASE_Pin */
   GPIO_InitStruct.Pin = H3_NSLEEP_Pin|H3_PHASE_Pin|H2_NSLEEP_Pin|H2_PHASE_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : PtPin */
+  /*Configure GPIO pin : H4_NSLEEP_Pin */
   GPIO_InitStruct.Pin = H4_NSLEEP_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(H4_NSLEEP_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : PtPin */
+  /*Configure GPIO pin : LED_Pin */
   GPIO_InitStruct.Pin = LED_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
