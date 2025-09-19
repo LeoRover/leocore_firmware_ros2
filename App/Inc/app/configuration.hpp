a1d9e0eb42282bb32961f905720deeb413769cb2
Aleksander Szymański <bitterisland6@gmail.com>
2025-09-19 18:25:46 +0200

    Disabling everything regarding motor controll
    Signed-off-by: Aleksander Szymański <bitterisland6@gmail.com>

--- App/Inc/app/configuration.hpp
+++ App/Inc/app/configuration.hpp
@@ -35,7 +35,7 @@ static constexpr I2C_HandleTypeDef& IMU_I2C = hi2c1;
 constexpr uint16_t PWM_RANGE = 1000;
 
 // Number of encoder readings to remember when estimating the wheel velocity
-constexpr uint32_t VELOCITY_ROLLING_WINDOW_SIZE = 10;
+// constexpr uint32_t VELOCITY_ROLLING_WINDOW_SIZE = 10;
 
 // The period (in number of calls to the update() function) at which the battery
 // voltage is probed
@@ -53,8 +53,8 @@ constexpr uint16_t UPDATE_PERIOD = 10;
 // The periods (in number of calls to the update() function) at which different
 // data is publihed on the ROS topics
 constexpr uint8_t BATTERY_PUB_PERIOD = 10;
-constexpr uint8_t JOINTS_PUB_PERIOD = 5;
-constexpr uint8_t ODOM_PUB_PERIOD = 5;
+// constexpr uint8_t JOINTS_PUB_PERIOD = 5;
+// constexpr uint8_t ODOM_PUB_PERIOD = 5;
 constexpr uint8_t IMU_PUB_PERIOD = 1;
 constexpr uint8_t PARAM_TRIGGER_PUB_PERIOD = 100;
 
@@ -73,103 +73,103 @@ constexpr float BATTERY_ADC_TO_VOLTAGE = 32.0F / 4095.0F;
 constexpr float VPROPI_ADC_TO_CURRENT = 2.5F / 4095.0F;
 
 // Motor driver configurations
-constexpr MotorConfiguration MOT_A_CONFIG = {
-    .nsleep = {H4_NSLEEP_GPIO_Port, H4_NSLEEP_Pin},
-    .phase = {H4_PHASE_GPIO_Port, H4_PHASE_Pin},
-    .mode = {H4_MODE_GPIO_Port, H4_MODE_Pin},
-    .fault = {H4_FAULT_GPIO_Port, H4_FAULT_Pin},
-    .enc_cnt = &TIM5->CNT,
-    .pwm_ccr = &TIM1->CCR4,
-    .vpropi_adc = &adc_buff[3],
-};
+// constexpr MotorConfiguration MOT_A_CONFIG = {
+//     .nsleep = {H4_NSLEEP_GPIO_Port, H4_NSLEEP_Pin},
+//     .phase = {H4_PHASE_GPIO_Port, H4_PHASE_Pin},
+//     .mode = {H4_MODE_GPIO_Port, H4_MODE_Pin},
+//     .fault = {H4_FAULT_GPIO_Port, H4_FAULT_Pin},
+//     .enc_cnt = &TIM5->CNT,
+//     .pwm_ccr = &TIM1->CCR4,
+//     .vpropi_adc = &adc_buff[3],
+// };
 
-constexpr MotorConfiguration MOT_B_CONFIG = {
-    .nsleep = {H3_NSLEEP_GPIO_Port, H3_NSLEEP_Pin},
-    .phase = {H3_PHASE_GPIO_Port, H3_PHASE_Pin},
-    .mode = {H3_MODE_GPIO_Port, H3_MODE_Pin},
-    .fault = {H3_FAULT_GPIO_Port, H3_FAULT_Pin},
-    .enc_cnt = &TIM4->CNT,
-    .pwm_ccr = &TIM9->CCR2,
-    .vpropi_adc = &adc_buff[2],
-};
+// constexpr MotorConfiguration MOT_B_CONFIG = {
+//     .nsleep = {H3_NSLEEP_GPIO_Port, H3_NSLEEP_Pin},
+//     .phase = {H3_PHASE_GPIO_Port, H3_PHASE_Pin},
+//     .mode = {H3_MODE_GPIO_Port, H3_MODE_Pin},
+//     .fault = {H3_FAULT_GPIO_Port, H3_FAULT_Pin},
+//     .enc_cnt = &TIM4->CNT,
+//     .pwm_ccr = &TIM9->CCR2,
+//     .vpropi_adc = &adc_buff[2],
+// };
 
-constexpr MotorConfiguration MOT_C_CONFIG = {
-    .nsleep = {H1_NSLEEP_GPIO_Port, H1_NSLEEP_Pin},
-    .phase = {H1_PHASE_GPIO_Port, H1_PHASE_Pin},
-    .mode = {H1_MODE_GPIO_Port, H1_MODE_Pin},
-    .fault = {H1_FAULT_GPIO_Port, H1_FAULT_Pin},
-    .enc_cnt = &TIM3->CNT,
-    .pwm_ccr = &TIM1->CCR1,
-    .vpropi_adc = &adc_buff[0],
-};
+// constexpr MotorConfiguration MOT_C_CONFIG = {
+//     .nsleep = {H1_NSLEEP_GPIO_Port, H1_NSLEEP_Pin},
+//     .phase = {H1_PHASE_GPIO_Port, H1_PHASE_Pin},
+//     .mode = {H1_MODE_GPIO_Port, H1_MODE_Pin},
+//     .fault = {H1_FAULT_GPIO_Port, H1_FAULT_Pin},
+//     .enc_cnt = &TIM3->CNT,
+//     .pwm_ccr = &TIM1->CCR1,
+//     .vpropi_adc = &adc_buff[0],
+// };
 
-constexpr MotorConfiguration MOT_D_CONFIG = {
-    .nsleep = {H2_NSLEEP_GPIO_Port, H2_NSLEEP_Pin},
-    .phase = {H2_PHASE_GPIO_Port, H2_PHASE_Pin},
-    .mode = {H2_MODE_GPIO_Port, H2_MODE_Pin},
-    .fault = {H2_FAULT_GPIO_Port, H2_FAULT_Pin},
-    .enc_cnt = &TIM2->CNT,
-    .pwm_ccr = &TIM9->CCR1,
-    .vpropi_adc = &adc_buff[1],
-};
+// constexpr MotorConfiguration MOT_D_CONFIG = {
+//     .nsleep = {H2_NSLEEP_GPIO_Port, H2_NSLEEP_Pin},
+//     .phase = {H2_PHASE_GPIO_Port, H2_PHASE_Pin},
+//     .mode = {H2_MODE_GPIO_Port, H2_MODE_Pin},
+//     .fault = {H2_FAULT_GPIO_Port, H2_FAULT_Pin},
+//     .enc_cnt = &TIM2->CNT,
+//     .pwm_ccr = &TIM9->CCR1,
+//     .vpropi_adc = &adc_buff[1],
+// };
 
-extern MotorController MotA;
-extern MotorController MotB;
-extern MotorController MotC;
-extern MotorController MotD;
+// extern MotorController MotA;
+// extern MotorController MotB;
+// extern MotorController MotC;
+// extern MotorController MotD;
 
 // Robot configuration for Leo Rover v1.8 or earlier
-constexpr diff_drive_lib::RobotConfiguration ROBOT_CONFIG_V1 = {
-    .wheel_FL_conf =
-        {
-            .motor = MotC,
-            .op_mode = diff_drive_lib::WheelOperationMode::VELOCITY,
-            .reversed = true,
-        },
-    .wheel_RL_conf =
-        {
-            .motor = MotD,
-            .op_mode = diff_drive_lib::WheelOperationMode::VELOCITY,
-            .reversed = true,
-        },
-    .wheel_FR_conf =
-        {
-            .motor = MotA,
-            .op_mode = diff_drive_lib::WheelOperationMode::VELOCITY,
-            .reversed = false,
-        },
-    .wheel_RR_conf =
-        {
-            .motor = MotB,
-            .op_mode = diff_drive_lib::WheelOperationMode::VELOCITY,
-            .reversed = false,
-        },
-};
+// constexpr diff_drive_lib::RobotConfiguration ROBOT_CONFIG_V1 = {
+//     .wheel_FL_conf =
+//         {
+//             .motor = MotC,
+//             .op_mode = diff_drive_lib::WheelOperationMode::VELOCITY,
+//             .reversed = true,
+//         },
+//     .wheel_RL_conf =
+//         {
+//             .motor = MotD,
+//             .op_mode = diff_drive_lib::WheelOperationMode::VELOCITY,
+//             .reversed = true,
+//         },
+//     .wheel_FR_conf =
+//         {
+//             .motor = MotA,
+//             .op_mode = diff_drive_lib::WheelOperationMode::VELOCITY,
+//             .reversed = false,
+//         },
+//     .wheel_RR_conf =
+//         {
+//             .motor = MotB,
+//             .op_mode = diff_drive_lib::WheelOperationMode::VELOCITY,
+//             .reversed = false,
+//         },
+// };
 
 // Robot configuration for Leo Rover v1.9 or later
-constexpr diff_drive_lib::RobotConfiguration ROBOT_CONFIG_V2 = {
-    .wheel_FL_conf =
-        {
-            .motor = MotA,
-            .op_mode = diff_drive_lib::WheelOperationMode::VELOCITY,
-            .reversed = true,
-        },
-    .wheel_RL_conf =
-        {
-            .motor = MotB,
-            .op_mode = diff_drive_lib::WheelOperationMode::VELOCITY,
-            .reversed = true,
-        },
-    .wheel_FR_conf =
-        {
-            .motor = MotC,
-            .op_mode = diff_drive_lib::WheelOperationMode::VELOCITY,
-            .reversed = false,
-        },
-    .wheel_RR_conf =
-        {
-            .motor = MotD,
-            .op_mode = diff_drive_lib::WheelOperationMode::VELOCITY,
-            .reversed = false,
-        },
-};
+// constexpr diff_drive_lib::RobotConfiguration ROBOT_CONFIG_V2 = {
+//     .wheel_FL_conf =
+//         {
+//             .motor = MotA,
+//             .op_mode = diff_drive_lib::WheelOperationMode::VELOCITY,
+//             .reversed = true,
+//         },
+//     .wheel_RL_conf =
+//         {
+//             .motor = MotB,
+//             .op_mode = diff_drive_lib::WheelOperationMode::VELOCITY,
+//             .reversed = true,
+//         },
+//     .wheel_FR_conf =
+//         {
+//             .motor = MotC,
+//             .op_mode = diff_drive_lib::WheelOperationMode::VELOCITY,
+//             .reversed = false,
+//         },
+//     .wheel_RR_conf =
+//         {
+//             .motor = MotD,
+//             .op_mode = diff_drive_lib::WheelOperationMode::VELOCITY,
+//             .reversed = false,
+//         },
+// };
